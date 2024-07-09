3c70e09e3d576d44c83494079751ad588676d000
Ananthu M A <heartchaserananthu@gmail.com>
2024-07-09 20:49:11 +0530

    Subheader updated

--- client/src/components/SubHeader.tsx
+++ client/src/components/SubHeader.tsx
@@ -1,13 +1,33 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const SubHeader = () => {
     return (
         <div className="w-full fixed top-16 left-0 bg-white border-b">
             <div className="flex flex-wrap justify-center md:justify-start w-full md:w-2/3 px-5 p-3 gap-5 md:gap-20">
-                <Link className="text-gray-600 border-b-2 border-transparent hover:text-blue-600 focus:text-blue-600 focus:border-blue-600 focus:font-bold" to="/">Home</Link>
-                <Link className="text-gray-600 border-b-2 border-transparent hover:text-blue-600 focus:text-blue-600 focus:border-blue-600 focus:font-bold" to="/my-learning">My Learning</Link>
-                <Link className="text-gray-600 border-b-2 border-transparent hover:text-blue-600 focus:text-blue-600 focus:border-blue-600 focus:font-bold" to="/online-courses">Online Courses</Link>
-                <Link className="text-gray-600 border-b-2 border-transparent hover:text-blue-600 focus:text-blue-600 focus:border-blue-600 focus:font-bold" to="/find-careers">Find Your New Career</Link>
+                <NavLink className={({ isActive }) => isActive
+                    ? "text-blue-600 border-b-2 border-blue-600 font-bold"
+                    : "text-gray-600 border-b-2 border-transparent hover:text-blue-600 focus:text-blue-600 focus:border-blue-600 focus:font-bold"
+                } to="/">
+                    Home
+                </NavLink>
+                <NavLink className={({ isActive }) => isActive
+                    ? "text-blue-600 border-b-2 border-blue-600 font-bold"
+                    : "text-gray-600 border-b-2 border-transparent hover:text-blue-600 focus:text-blue-600 focus:border-blue-600 focus:font-bold"
+                } to="/my-learning">
+                    My Learning
+                </NavLink>
+                <NavLink className={({ isActive }) => isActive
+                    ? "text-blue-600 border-b-2 border-blue-600 font-bold"
+                    : "text-gray-600 border-b-2 border-transparent hover:text-blue-600 focus:text-blue-600 focus:border-blue-600 focus:font-bold"
+                } to="/online-courses">
+                    Online Courses
+                </NavLink>
+                <NavLink className={({ isActive }) => isActive
+                    ? "text-blue-600 border-b-2 border-blue-600 font-bold"
+                    : "text-gray-600 border-b-2 border-transparent hover:text-blue-600 focus:text-blue-600 focus:border-blue-600 focus:font-bold"
+                } to="/find-careers">
+                    Find Your New Career
+                </NavLink>
             </div>
         </div>
     );
